d2e0ece90d4830ae6a3398368b18135034c041de
Christopher Devalme <cdevalme@gmail.com>
2023-11-19 20:14:59 -0500

    new

---
+++ Week9HW-DOM/js.js
@@ -0,0 +1,14 @@
+function reword(word) {
+    let space = " ";
+    let newword = "";
+    let str = word.splice(" ");
+  
+    for (i = 0; i < word.char(i); i++) {
+      if (str === " ") {
+        word.splice
+        newword.push()
+      }
+    }
+  
+}
+console.log("Hello world! This is a test.")
